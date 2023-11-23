ce0409e1f486ea1afa8148f78e5902efb031f165
Ivan Antonenko <ivan.antonenko@proton.me>
2023-11-23 23:09:45 +0100

    add styles and create elements refs in js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const refs = {
+  gameTitle: document.querySelector(".game__title"),
+  playerScore: document.querySelector("#player-score"),
+  computerScore: document.querySelector("#computer-score"),
+  gameChoises: document.querySelector(".game__choice-list"),
+  gameButtons: document.querySelector(".game__buttons-list"),
+  winnerTitle: document.querySelector(".game__winner-title"),
+  restartButton: document.querySelector(".game__restart-button"),
+};
